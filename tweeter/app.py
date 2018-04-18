8d08fe15fc9402f7a280a04e8bbd146287118f9e
Richard Li <richard@datawire.io>
2018-04-18 11:30:05 -0400

    send random quotes

--- tweeter/app.py
+++ tweeter/app.py
@@ -2,25 +2,30 @@
 
 import time
 import requests
-import json
+import json, os
 import datetime
 
 # Infinite loop; we can use `kubectl scale` to turn this on and off
 
 url = 'http://35.195.162.45/stream/add-quotes/'
 # url = 'http://localhost:8080/add-quotes/'
-quote = { 'author': 'Albert Einstein', 'text': 'Imagine is more important than knowledge.'}
-
-# https://github.com/4skinSkywalker/Database-Quotes-JSON
 
 def main():
 
-    quote['created_at'] = datetime.datetime.now().isoformat()
+    with open('quotes.json') as json_file:
+        json_data = json.load(json_file)
 
     while True:
-        print('sending payload', json.dumps(quote,indent=2))
-        r = requests.post(url,json=quote)
-        time.sleep(0.5)
+        tweet = { }
+
+        for quote in json_data:
+            tweet['author'] = quote['quoteAuthor']
+            tweet['text'] = quote['quoteText']
+            tweet['created_at'] = datetime.datetime.now().isoformat()
+
+            r = requests.post(url,json=tweet)
+            time.sleep(0.5)
+
 
 if __name__ == "__main__":
     main()
