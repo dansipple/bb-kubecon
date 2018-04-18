5b9dd14eb36427ae7d2fbb86aa6b50fcecb1d1e3
Richard Li <richard@datawire.io>
2018-04-18 11:02:39 -0400

    add persistence

--- stream/app.py
+++ stream/app.py
@@ -3,6 +3,8 @@
 import requests
 import datetime
 import json
+import sqlite3
+import os
 
 from flask import Flask, request, jsonify
 
@@ -10,20 +12,51 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-quote = { 'author': 'Albert Einstein', 'created_at': '2012-04-05', 'text': 'Imagine is more important than knowledge.'}
-quote2 = { 'author': 'Albert Einstein', 'created_at': '2012-04-05', 'text': 'Imagine is more important than knowledgs.'}
-
-tweets = [quote, quote2]
-
 @app.route('/get-quotes/')
 def get_quotes():
+    conn = sqlite3.connect('tweet.db')
+    c = conn.cursor()
+
+    c.execute('select author, created_at, text from tweets limit 5')
+    rows = c.fetchall()
+    
+    tweet = { }
+    tweets = [ ]
+
+    for row in rows:
+        tweet['author'] = row[0]
+        tweet['created_at'] = row[1]
+        tweet['text'] = row[2]
+        tweets.append(tweet)
+
     return jsonify(tweets)
 
 @app.route('/add-quotes/', methods=['POST'])
 def add_quotes():
-    foo = request.get_json()
-    print('hello',foo)
+    tweet = request.get_json()
+
+    if os.path.exists('tweet.db')==False:
+        init()
+
+    author = tweet['author']
+    created_at = tweet['created_at']
+    quote = tweet['text']
+
+    conn = sqlite3.connect('tweet.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO tweets VALUES(?, ?, ?)',
+             (author, created_at, quote))
+    print('saved tweet', tweet)
+    conn.commit()
+    conn.close()
+
     return "Received"
+
+def init():
+    conn = sqlite3.connect('tweet.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE tweets (author text, created_at text, text text)')
+    conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
